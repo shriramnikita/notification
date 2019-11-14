47ecc962ea8a96bb66cf2df4fae0930cc6e7738c
unknown <dhanashri0912@gmail.com>
2019-11-14 14:52:10 -0800

    first commit

---
+++ Notification/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Notification
+{
+    public class NotesModel
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        /// <value>
+        /// The created date.
+        /// </value>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the modified date.
+        /// </summary>
+        /// <value>
+        /// The modified date.
+        /// </value>
+        public DateTime ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+
+        [ForeignKey("ApplicationRegisterModel")]
+        public string UserId { get; set; }
+
+    
+        /// <summary>
+        /// Gets or sets the color.
+        /// </summary>
+        /// <value>
+        /// The color.
+        /// </value>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets the image.
+        /// </summary>
+        /// <value>
+        /// The image.
+        /// </value>
+       // [Column(TypeName = "image")]
+        public string Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [pin unpin].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [pin unpin]; otherwise, <c>false</c>.
+        /// </value>
+   ////     public bool Pin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reminders.
+        /// </summary>
+        /// <value>
+        /// The reminders.
+        /// </value>
+        public DateTime Reminders { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is pin.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is pin; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsPin { get; set; }
+    }
+}
---
+++ Notification/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Json.Net;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Notification
+{
+    class Program
+    {
+        // static readonly HttpClient client = new HttpClient();
+        static async Task CallWebAPIAsync()
+        {
+            //using (var response = request.GetResponse() as HttpWebResponse)
+            //{
+            // var request = WebRequest.Create("http://localhost:12698/token") as HttpWebRequest;
+            // request.CookieContainer = new CookieContainer();
+            // authCookie = response.Cookies["access_token"];
+            //}
+            using (var client = new HttpClient())
+            {
+                string userId = "7212c15b-3921-4104-a078-fa2a1409844a";
+                var response = await client.GetAsync("https://localhost:44377/api/Notes/alarm/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+                var responseAsString = await response.Content.ReadAsStringAsync();
+             //   var responseAsConcreteType = JsonConverter.DeserializeObject<NotesModel>(responseAsString);
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                Console.WriteLine(responseBody);
+            }
+            //catch (HttpRequestException e)
+            //{
+            // Console.WriteLine("\nException Caught!");
+            // Console.WriteLine("Message :{0} ", e.Message);
+            //}
+        }
+        static void Main(string[] args)
+        {
+            CallWebAPIAsync()
+            .Wait();
+            Console.WriteLine("Hello I am trying to calling push notification web api........!");
+            System.Threading.Thread.Sleep(1000 * 60 * 5);
+        }
+    }
+}
